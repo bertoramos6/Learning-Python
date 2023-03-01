41b8b9b7a16e9d02dad98d6dec572bef03cd2bae
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2023-03-01 11:46:33 +0100

    Create 01_variables.py

---
+++ Basic/01_variables.py
@@ -0,0 +1,9 @@
+# Variables
+
+#en Python usaremos snake case por convención
+string_variable = "My String variable"
+int_variable = 3
+bool_variable = True
+
+#print puede recibir varios argumentos (espacia entre cada argumento)
+print(type(string_variable, int_variable, bool_variable)) 
